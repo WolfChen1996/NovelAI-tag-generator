5e7f0bc28d5cfffc0070de328f0dd5a32f37d07e
WolfChen1996 <784518175@qq.com>
2022-11-03 18:37:07 +0800

    更新至V2.1

--- index_files/post.js
+++ index_files/post.js
@@ -1,18 +1,33 @@
 function submittag(){
-var rawtag=document.getElementById("tagarea").value;
-if(rawtag==""){}else{
-    var formData = rawtag;
-    $.ajax({
-    	type: "POST",
-    	url: "./note.php",
-    	data: formData,
-    	processData: false,// 不加会报错
-    	contentType: false,// 不加会报错
-    	success: function (res) {
-    		console.log(res);
-    	}
-    });
+var formData = document.getElementById("tagarea").value;
+if(formData==""){}
+else{
+    if(document.domain=="wolfchen.top" || document.domain=="wjingnan.com" )
+    {
+        $.ajax({
+        	type: "POST",
+        	url: "./note.php",
+        	data: formData,
+        	processData: false,// 不加会报错
+        	contentType: false,// 不加会报错
+        	success: function (res) {
+        		console.log(res);
+        	}
+        });
+    }
+    else
+    {
+        $.ajax({
+        	type: "POST",
+        	url: "https://wolfchen.top/tag/othernote.php",
+        	data: formData,
+        	processData: false,// 不加会报错
+        	contentType: false,// 不加会报错
+        	success: function (res) {
+        		console.log(res);
+        	}
+        });
+    }
     
-    play();
     }
 }
